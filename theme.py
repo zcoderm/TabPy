1de211b04d13baec53956098a496ccb13f7e176c
zcoderm <z.coder.m@gmail.com>
2020-03-31 11:08:39 +0100

    Update theme.py

--- theme.py
+++ theme.py
@@ -1,1 +1,9 @@
-
+import pandas as pd
+theme = pd.read_csv("Theme.csv")
+theme_list = []
+theme_to_list = theme.values.tolist()
+for value in theme_to_list:
+    for i in value:
+        i = i[1:-1].split(",")
+        theme_list += i
+theme_list
